46fe0e5e3cf91e0b539182e7da425613c028a428
Ryan Leckey <ryan@launchbadge.com>
2016-08-10 23:16:07 -0700

    Initial Commit

---
+++ RNUserAgent/RNUserAgent.h
@@ -0,0 +1,13 @@
+//
+//  RNUserAgent.h
+//  RNUserAgent
+//
+//  Created by Ryan Leckey on 8/10/16.
+//  Copyright © 2016 Ryan Leckey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNUserAgent : NSObject
+
+@end
---
+++ RNUserAgent/RNUserAgent.m
@@ -0,0 +1,13 @@
+//
+//  RNUserAgent.m
+//  RNUserAgent
+//
+//  Created by Ryan Leckey on 8/10/16.
+//  Copyright © 2016 Ryan Leckey. All rights reserved.
+//
+
+#import "RNUserAgent.h"
+
+@implementation RNUserAgent
+
+@end
